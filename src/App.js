3937c967d34d79f518190efff734e604fa41d630
Milos Stojanovic <stojanovic98m@gmail.com>
2022-06-23 14:23:51 +0200

    saturn components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import ColorPallete from "./components/ColorPallete";
-import Flower from "./components/Flower";
-import Rocket from "./components/Rocket";
+//import Flower from "./components/Flower";
+//import Rocket from "./components/Rocket";
+import Saturn from "./components/Saturn";
 import "./app.css";
 function App() {
   //colors
@@ -56,7 +57,7 @@ function App() {
           <h1 className="title">Coloring Book </h1>
         </section>
         <section className="svg-container">
-          <Flower colors={colors} fill={fillColors} dimensions={dimensions} />
+          <Saturn colors={colors} fill={fillColors} dimensions={dimensions} />
         </section>
         <section className="pallete-container">
           <ColorPallete
